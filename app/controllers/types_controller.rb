9842fa54f56f17a9ab4025ff8ae7c48e4a29c2f1
John Madden <jck13mad@gmail.com>
2021-02-13 17:24:22 -0600

    Filled in methods for type controller

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,23 +1,30 @@
 class TypesController < ApplicationController
 
     def new 
-        
+        @type = Type.new 
     end
 
     def create 
+        @type = Type.new(type_params)
+        if @type.save 
+            redirect_to type_path(@type)
+        else
+            render :new 
+        end
     end 
 
     def show
+        @type = Type.find(params[:id])
+        @plants = Plant.all.where(Plant.type == @type)
     end
 
     def index
+        @types = Type.all 
     end
-
-    def show 
-    end 
 
     private 
 
         def type_params
+            params.require(:type).permit(:name, plants_attributes: [:name, :description, :img])
         end
 end
