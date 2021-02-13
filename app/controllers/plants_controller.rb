bee8107d7676040778922fd8980b35474bc86a1c
John Madden <jck13mad@gmail.com>
2021-02-13 17:05:55 -0600

    Made methods for plant controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,32 @@
 class PlantsController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit 
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private 
+
+        def plant_params
+        end
+
+        def find_plant 
+        end 
+
+        def redirect_if_not_owner
+            
+        end 
 end
