57bb0adc310f210d02c2ce08760e796b652ddb39
John Madden <jck13mad@gmail.com>
2021-02-13 16:54:37 -0600

    Filled in methods in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,25 +13,60 @@ class CommentsController < ApplicationController
     end 
 
     def new
+        if params[:plant_id] && @plant = Plant.find_by_id([:plant_id])
+            @comment = @plant.comments.new 
+        else
+            @error = "Plant does not exist" if params[:plant_id]
+            @comment = Comment.new 
+        end
     end 
     
-    def create 
+    def create
+        @plant = Plant.find(params[:comment][:plant_id])
+        @comment = current_user.comments.new(comment_params)
+        
+        if @comment.save
+            redirect_to plant_comment_path(@plant, @comment)
+        else
+            @error = @comment.errors.full_messages
+            render :new 
+        end
     end 
 
-    def show 
+    def show
+        @comment = Comment.find(params[:id]) 
     end 
 
     def edit
+        @comment = Comment.find(params[:id])
     end 
 
     def update
+        @comment = current_user.comments.find(params[:id])
+
+        if @comment.update(comment_params)
+            redirect_to comment_path(@comment)
+        else
+            @error = @comment.errors.full_messages
+            render :edit 
     end 
 
     def destroy
+        @comment = current_user.comments.find(params[:id])
+        @plant = Plant.find(params[:comment][:plant_id]) 
+
+        if @comment.destroy 
+            flash[:success] = "Your comment was removed."
+            redirect_to plants_path(@plant)
+        else
+            @error = @comment.errors.full_messages
+            render :edit 
+        end 
     end 
 
     private 
 
         def comment_params
+            params.require(:content).permit(:content, :plant_id, :user_id)
         end 
 end
