390ef467c6c8c251b890f888906e07ff7a576241
John Madden <jck13mad@gmail.com>
2021-02-13 17:19:39 -0600

    Made methods for type controller

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,2 +1,23 @@
 class TypesController < ApplicationController
+
+    def new 
+        
+    end
+
+    def create 
+    end 
+
+    def show
+    end
+
+    def index
+    end
+
+    def show 
+    end 
+
+    private 
+
+        def type_params
+        end
 end
