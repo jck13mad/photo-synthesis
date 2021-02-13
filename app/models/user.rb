3cda9a8d5b197df28d182e14a6e0377a549d4857
John Madden <jck13mad@gmail.com>
2021-02-13 16:04:45 -0600

    Set up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :plants 
+    has_many :types, through: :plants 
+    validates :email, uniqueness: true 
+    validates :username, :email, presence: true 
+
+    def self.create_from_omniauth(auth)
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.username = auth['info']['first_name']
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex(18)
+        end
+    end 
+
 end
