53c000e0bf3e23be479536fd1e7daadf5d473f59
John Madden <jck13mad@gmail.com>
2021-02-13 16:46:42 -0600

    Layed out methods for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,37 @@
 class CommentsController < ApplicationController
+    before_action :redirect_if_not_logged_in 
+
+    def index
+        if params[:plant_id] && @plant = Plant.find_by_id(params[:plant_id])
+            @comment = @recipe.comments 
+        elsif current_user
+            @comment = current_user.comments.all 
+        else
+            @error = "Plant does not exist" if params[:plant_id]
+            @comments = Comment.all 
+        end
+    end 
+
+    def new
+    end 
+    
+    def create 
+    end 
+
+    def show 
+    end 
+
+    def edit
+    end 
+
+    def update
+    end 
+
+    def destroy
+    end 
+
+    private 
+
+        def comment_params
+        end 
 end
