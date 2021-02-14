d730ce38da3db1026a99543b97d71dfb22c098bf
John Madden <jck13mad@gmail.com>
2021-02-14 15:56:36 -0600

    Set root page to sessions/welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  resources :types
+  root 'sessions#welcome'
+
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  resources :types, only: [:new, :create, :index, :show] do
+    resources :plants, only: [:new, :create, :index]
+  end 
   resources :comments
   resources :users
   resources :plants
