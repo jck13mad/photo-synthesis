acff03c129f7098d371590d1630e1415f08eb207
John Madden <jck13mad@gmail.com>
2021-02-14 19:42:37 -0600

    Added display type fields in plants heler

--- app/helpers/plants_helper.rb
+++ app/helpers/plants_helper.rb
@@ -1,2 +1,8 @@
 module PlantsHelper
+    def display_type_fields(t) 
+        if params[:type_id]
+            t.hidden_field :type_id
+        else
+            render partial: 'type_select', locals: {f: t}
+    end 
 end
