425237e53f959949514ae74e03b0a8eabd1cbb21
John Madden <jck13mad@gmail.com>
2021-02-14 19:01:58 -0600

    Added google client secrets to a .env and added omniauth.rb to the initializer folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Plant.create ([
-    {name: "Kitchen Plant", description: "This is the plant that I have hanging in my kitchen.", img: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2019%2F03%2F22185851%2Fstring-of-beads-hanging-plant-44db0b32.jpg&q=85", user_id: 1}
+    name: "Window Plant", description: "I have this plant hanging in the window.", img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/concept-of-home-gardening-spathiphyllum-in-royalty-free-image-1580918113.jpg", user_id: 1
 ]
 
 )
